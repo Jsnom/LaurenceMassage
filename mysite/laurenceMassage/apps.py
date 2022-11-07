272b19a7d66c3ca000b8c1cdf0ebe9c2619fdff1
Marvin Gousset <m7gousse@enib.fr>
2022-11-07 02:04:34 +0100

    Delete mysite directory

--- mysite/laurenceMassage/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LaurencemassageConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'laurenceMassage'
--- mysite/laurenceMassage/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-
-def index(request):
-    context = {}
-    return render(request, 'laurenceMassage/index.html', context)
